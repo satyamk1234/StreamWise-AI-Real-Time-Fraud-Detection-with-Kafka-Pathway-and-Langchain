7ec5af73aa28e3afda5f8358a622b7216e009923
satyamk1234 <sk01satyamkumar@gmail.com>
2025-10-23 18:08:02 +0530

    langchain_service

--- langchain_service.py
+++ langchain_service.py
@@ -2,13 +2,11 @@ from kafka import KafkaConsumer, KafkaProducer
 from langchain_openai import ChatOpenAI
 import json, os
 
-# --- Load your OpenAI key ---
+# Load your OpenAI key 
 os.environ["OPENAI_API_KEY"] = "sk-proj---->
 
-# --- Initialize the model ---
 llm = ChatOpenAI(model="gpt-4o-mini")
 
-# --- Connect to Kafka ---
 consumer = KafkaConsumer(
     "alerts",
     bootstrap_servers="localhost:9092",
@@ -22,21 +20,15 @@ producer = KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode("utf-8")
 )
 
-print("🚀 LangChain service started. Waiting for alerts...")
+print("LangChain service started. Waiting for alerts...")
 
-# --- Main processing loop ---
+# Main processing loop 
 for msg in consumer:
     alert = msg.value
     txn_str = json.dumps(alert, indent=2)
-
-    # Generate AI explanation
     prompt = f"Explain briefly why this transaction might be suspicious:\n{txn_str}"
     explanation = llm.invoke(prompt)
-
-    # Add explanation to message
     alert["ai_explanation"] = explanation.content
-    print("💡 AI explanation added:", alert["ai_explanation"])
-
-    # Send back into Kafka (topic ai_alerts)
+    print("AI explanation added:", alert["ai_explanation"])
     producer.send("ai_alerts", alert)
     producer.flush()
