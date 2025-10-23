ebb735c30a0f4811cfe802e04d8957922c004de3
satyamk1234 <sk01satyamkumar@gmail.com>
2025-10-23 16:13:20 +0530

    Add files via upload

---
+++ producer.py
@@ -0,0 +1,21 @@
+from kafka import KafkaProducer
+import json, random, time
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+print("🚀 Sending simulated transactions to Kafka...")
+
+while True:
+    txn = {
+        "txn_id": random.randint(10000, 99999),
+        "user_id": random.randint(1, 50),
+        "amount": round(random.uniform(10, 1000), 2),
+        "country": random.choice(["US", "UK", "IN", "SG"]),
+        "timestamp": time.time()
+    }
+    producer.send("transactions", txn)
+    print("Sent:", txn)
+    time.sleep(1)
